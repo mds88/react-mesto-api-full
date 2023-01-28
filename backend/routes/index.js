140acea69825ce72d06d894d2dcbbfde7b338009
Dmitrii Malyshev <malyshevds88@yandex.ru>
2023-01-28 13:33:33 +0800

    feat: ПР15

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,6 +9,12 @@ const routerCard = require('./cards');
 const { regexp } = require('../utils/constants');
 const NotFoundError = require('../errors/NotFoundError');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
--- frontend/src/utils/mestoAuth.js
+++ frontend/src/utils/mestoAuth.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://api.immortal.nomoredomains.club';
 
 function registr(email, password) {
     return fetch(`${BASE_URL}/signup`, {
