ae9459af7e5e49eb7f0fc377b3c799c2bbf2cbba
Dmitrii Malyshev <malyshevds88@yandex.ru>
2023-01-28 10:39:39 +0800

    feat: ПР15

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
-// const cors = require('cors');
 const cors = require('./middlewares/cors');
 const indexRouter = require('./routes/index');
 const errorHandler = require('./middlewares/errorHandler');
@@ -16,10 +15,6 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use(cors({
-//   origin: 'http://localhost:3001',
-//   credentials: true,
-// }));
 app.use(cors);
 
 mongoose.set('strictQuery', true);
